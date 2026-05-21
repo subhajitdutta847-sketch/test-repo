a00f538ab805152e76665ad59a914a637c3ca150
subhajitdutta847-sketch <subhajitdutta847@gmail.com>
2026-05-21 14:09:10 +0200

    Reciprocate

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-def prod_intergers(  num3, num4):
- return num3 * num4
+# Main.py
 
+"""This module for blablabla"""
+
+import double_it    # first way to import
+
+# from double_it import double_it
+
+X_INIT: int = 1
+
+
+def prod_integers(old_inventory: int, new_inventory: int
+                  ) -> int:
+    """_summary_
+
+    :param num1: _description_
+    :type num1: dict[str, int]
+    :param num2: _description_
+    :type num2: list[int]
+    :return: _description_
+    :rtype: int
+    """
+
+    y = old_inventory + new_inventory + X_INIT
+
+    result = double_it.double_int(y)
+
+    return result
+
+
+if __name__ == "__main__":
+    print(prod_integers(1, 2))
